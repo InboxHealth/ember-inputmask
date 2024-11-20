3b345ef11aabd563e90a12d84591d2b9ef1524e8
Richard Stovall <richard.stovall@inboxhealth.com>
2024-11-20 12:01:57 -0500

    ...

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   name: require('./package').name,
-  included(app) {
+  included: function (app) {
     this._super.included(app);
 
-    this.import('node_modules/jquery.inputmask/dist/jquery.inputmask.bundle.js');
+    app.import('node_modules/jquery.inputmask/dist/jquery.inputmask.bundle.js');
   },
 };
